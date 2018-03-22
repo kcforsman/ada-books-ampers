4acf86e298fe38888d68edf42fc118fb55f93248
Kaitlin Forsman <kcforsman@outlook.com>
2018-03-22 10:07:13 -0700

    added update and edit methods and layouts

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,7 @@
 class BooksController < ApplicationController
 
   def index
-    @books = Book.all
+    @books = Book.all.order(:author)
   end
 
   def show
@@ -10,15 +10,39 @@ class BooksController < ApplicationController
   end
 
   def new
+    @book = Book.new
   end
 
   def create
+    book = Book.new
+    book.title = params[:book][:title]
+    book.author = params[:book][:author]
+    book.description = params[:book][:description]
+    if book.save
+      redirect_to books_path
+    else
+      render :new
+    end
   end
 
   def edit
+    id = params[:id]
+    @book = Book.find(id)
   end
 
   def update
+    id = params[:id]
+    book = Book.find(id)
+    if !book.nil?
+      book.title = params[:book][:title]
+      book.author = params[:book][:author]
+      book.description = params[:book][:description]
+      if book.save
+        redirect_to book_path(id)
+      else
+        render :edit
+      end
+    end
   end
 
   def destroy
